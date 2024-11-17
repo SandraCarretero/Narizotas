5ad967c816a9dac9bbf52d72c5e27bafe269d66c
SandraCarretero <sandracarretero24@gmail.com>
2024-11-17 20:58:40 +0100

    input

--- src/pages/product/product.styles.js
+++ src/pages/product/product.styles.js
@@ -126,6 +126,7 @@ const StyledInput = styled.select`
 	color: #000;
 	width: 100%;
 	box-sizing: border-box;
+	background-color: white;
 
 	padding-right: 24px;
 	background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 24 24' fill='none' stroke='%23000' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3E%3Cpath d='M6 9l6 6 6-6'%3E%3C/path%3E%3C/svg%3E");
